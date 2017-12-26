e3180bd7e12fc7fb933d30eeb52115fe9d6b467a
nov3o <ilya.bad111@yandex.ru>
2017-12-26 23:48:11 +0300

    A little fix for dialogue block

--- mysite/settings.py
+++ mysite/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'a2^%25mew+=%h$#2ly5gn)#o*fc@lu$rb#_#t_madi47a2or=8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['http://127.0.0.1:8000']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
